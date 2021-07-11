39e7e43dc5fbe85fa6a2f9ff6b07e552561719b3
songujin <songuzin@naver.com>
2021-07-11 21:50:34 +0900

    AOP 적용

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,4 +1,5 @@
 package hello.hellospring;
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +36,12 @@ public class SpringConfig {
     public MemberService memberService() {
         return new MemberService(memberRepository);
     }
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
+
     //@Bean
     //public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -29,18 +29,9 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member) {
-
-        long start = System.currentTimeMillis();
-
-        try {
-            ValidateDuplicateMemter(member); //중복 회원 검증
-            memberRepository.save(member);
-            return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join =" + timeMs + "ms");
-        }
+        ValidateDuplicateMemter(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
     }
 
     private void ValidateDuplicateMemter(Member member) {
@@ -54,15 +45,7 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        long start = System.currentTimeMillis();
-        try{
-            return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findMembers =" + timeMs + "ms");
-        }
-
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId) {
